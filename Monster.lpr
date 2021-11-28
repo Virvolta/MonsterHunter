eafed0a1743902b5baf73f5c01bd15530f323f9b
1ZFly1 <pecnikcyrille@gmail.com>
2021-11-28 21:25:13 +0100

    JSON LE HERISSON

--- Monster.lpr
+++ Monster.lpr
@@ -4,8 +4,11 @@ uses menu, IHM, logique, uniteCantine, uniteChambre, uniteMarchand,
   controle, uniteforge, personnage, uniteChasse, outils;
 
 begin
-  start();
+  //start();
   //menuJeu();
+  ParseFile('json/objets.json','objets');
+  ParseFile('json/equipements.json','equipements');
+  ParseFile('json/equipements.json','lol');
   readln();
 end.
 
--- outils.pas
+++ outils.pas
@@ -4,6 +4,31 @@ unit outils;
 
 interface
 
+uses
+  fpjson, jsonparser;
+
+type
+  item=record
+    id:Integer;
+    nom:String;
+    prixAchat:Integer;
+    prixVente:Integer;
+  end;
+  equipment=record
+    id:Integer;
+    nom:String;
+    stat:Integer;
+  end;
+
+var
+
+  tabItems : array [0..5] of item;
+  tabEquipments : array [0..17] of equipment;
+
+procedure displayData(data : TJSONData; ObjName : String);
+procedure DoParse(Parseur : TJSONParser ; ObjName : String);
+procedure ParseFile(FileName, ObjName : String);
+
 procedure ascii(nom : string; x, y : integer);
 procedure animationdeplacement(x, y, x2, y2 : integer);
 
@@ -79,6 +104,96 @@ begin
     write(char(219),char(219));
 
   until anime = false;
+end;
+
+procedure displayData(data : TJSONData ; ObjName : String);
+ var
+   obj : TJSONObject;
+   tab : TJSONArray;
+   i : byte;
+
+begin
+  obj :=TJSONObject(data);
+  {writeln('Les caracteristiques du langage : '+obj.Strings['langage']);
+  writeln('Date de creation : ',lang.Integers['annee_creation']);}
+  writeln('------------- Les objets disponibles ---------------- ');
+  if ObjName='objets' then
+  begin
+     tab:=obj.Arrays[ObjName];
+     for i:=0 to tab.Count-1 do
+     begin
+       obj:= tab.Objects[i];
+       tabItems[i].id:=StrToInt(obj.Strings['id']);
+       tabItems[i].nom:=obj.Strings['nom'];
+       tabItems[i].prixAchat:=StrToInt(obj.Strings['prixAchat']);
+       tabItems[i].prixVente:=StrToInt(obj.Strings['prixVente']);
+     end;
+
+     for i:=0 to Length(tabItems) do
+     begin
+       write('id : ',tabItems[i].id);
+       write(' / ');
+       write('nom : ',tabItems[i].nom);
+       write(' / ');
+       write('prixAchat : ',tabItems[i].prixAchat);
+       write(' / ');
+       writeln('prixVente : ',tabItems[i].prixAchat);
+     end;
+  end
+  else if ObjName='equipements' then
+  begin
+     tab:=obj.Arrays['equipements'];
+     for i:=0 to tab.Count-1 do
+     begin
+       obj:= tab.Objects[i];
+       tabEquipments[i].id:=StrToInt(obj.Strings['id']);
+       tabEquipments[i].nom:=obj.Strings['nom'];
+       tabEquipments[i].stat:=StrToInt(obj.Strings['stat']);
+     end;
+
+     for i:=0 to Length(tabEquipments) do
+     begin
+       write('id : ',tabEquipments[i].id);
+       write(' / ');
+       write('nom : ',tabEquipments[i].nom);
+       write(' / ');
+       writeln('stat : ',tabEquipments[i].stat);
+     end;
+  end
+  else
+      writeln('Aucun objet de ce type dans le fichier JSON');
+  writeln('---------------------------------------');
+end;
+
+Procedure DoParse(Parseur : TJSONParser ; ObjName : String);
+Var
+  js : TJSONData;
+begin
+  js:=parseur.Parse;
+  displayData(js,ObjName);
+end;
+
+Procedure ParseFile (FileName, ObjName : String);
+Var
+  flux : TFileStream;
+  Parseur : TJSONParser;
+begin
+  If FileExists(FileName) then
+  begin
+    flux:=TFileStream.create(FileName, fmopenRead);
+      try
+        parseur:=TJSONParser.create(flux);
+        try
+          DoParse(parseur, ObjName);
+        finally
+          FreeAndNil(parseur);
+        end;
+      finally
+        flux.Destroy;
+      end;
+  end
+  else
+      writeln('Le fichier JSON : ', FileName,' n''existe pas');
 end;
 
 end.
