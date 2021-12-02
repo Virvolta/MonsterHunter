acaddea71d62e53dd8937d78ab08c8db5cd1f43d
1ZFly1 <pecnikcyrille@gmail.com>
2021-12-02 10:42:26 +0100

    Revert "Merge branch 'main' of https://github.com/Virvolta/MonsterHunter"
    This reverts commit 9aba5e9c8c3b7fb5c91ab22c6d677da2e1396c65, reversing
    changes made to 3a1be4687503596c9f3c9f0256b7b7eff8898fab.

--- unitechambre.pas
+++ unitechambre.pas
@@ -134,58 +134,26 @@ function menuArmoire():Integer;
 var
    i:Integer;
    inv : TypeInventaire;
-   arm : TypeArmoire;
    pos : coordonnees;
    it : Item;
-   select,select2 : Integer;
+   select : Integer;
    ch : char;
-   count,count2 : Integer;
-   posinv : array[0..NOMBRE_INV_JEU] of Integer;
-   posarm : array[0..NOMBRE_ARMOIRE_JEU] of Integer;
-   inventory : boolean;
-
 begin
   effacerEcran();
-  inventory := true;
-  select := 1;
   couleurs(15, 0);
   ascii('armoire', 0, 0);
   pos.x := 3;
-  pos.y := 2;
-  count := 0;
+  pos.y := 1;
   inv := getInventory();
   for i := Low(inv) to High(inv) do
       begin
         it := getItemInventory(i);
         if ((it.id > 0) or (it.count > 0)) then
            begin
-              if (select = i) then
-                 couleurs(0, 15)
-              else
-                 couleurs(15, 0)
-              ;
-              count := count + 1;
-              posinv[count] := i;
               ecrireEnPosition(pos, concat(tabProduits[tabIdProduits[it.id]].nom, ' x', InttoStr(it.count)));
               pos.y := pos.y + 1;
            end;
       end;
-  pos.x := 70;
-  pos.y := 2;
-  count2 := 0;
-  arm := getArmoire();
-    for i := Low(arm) to High(arm) do
-      begin
-        it := getItemArmoire(i);
-        if ((it.id > 0) or (it.count > 0)) then
-           begin
-              count2 := count2 + 1;
-              posarm[count2] := i;
-              ecrireEnPosition(pos, concat(tabProduits[tabIdProduits[it.id]].nom, ' x', InttoStr(it.count)));
-              pos.y := pos.y + 1;
-           end;
-      end;
-  couleurs(15, 0);
   pos.x := 58;
   pos.y := 27;
   ecrireEnPosition(pos, 'Menu');
@@ -193,255 +161,7 @@ begin
   select := 1;
   repeat
     ch := ReadKey;
-    case ch of
-      'M','K':
-      begin
-        select := 1;
-        pos.x := 3;
-        pos.y := 2;
-        for i := Low(inv) to High(inv) do
-        begin
-        it := getItemInventory(i);
-        if ((it.id > 0) or (it.count > 0)) then
-           begin
-              ecrireEnPosition(pos, concat(tabProduits[tabIdProduits[it.id]].nom, ' x', InttoStr(it.count)));
-              pos.y := pos.y + 1;
-           end;
-        end;
-        pos.x := 70;
-        pos.y := 2;
-        for i := Low(arm) to High(arm) do
-        begin
-        it := getItemArmoire(i);
-        if ((it.id > 0) or (it.count > 0)) then
-           begin
-              ecrireEnPosition(pos, concat(tabProduits[tabIdProduits[it.id]].nom, ' x', InttoStr(it.count)));
-              pos.y := pos.y + 1;
-           end;
-        end;
-        if (inventory = true) then
-           inventory := false
-        else
-           inventory := true;
-      end;
-      'P':
-      begin
-        if (inventory = true) then
-          begin
-             select := select + 1;
-             if (select > count + 1) then
-             select := 1
-          end
-        else
-          begin
-             select := select + 1;
-             if (select > count2 + 1) then
-             select := 1
-          end  ;
-      end;
-      'H':
-      begin
-        if (inventory = true) then
-          begin
-           select := select - 1;
-           if (select <= 0) then
-              select := count + 1;
-          end
-        else
-          begin
-           select := select - 1;
-           if (select <= 0) then
-              select := count2 + 1;
-          end;
-      end;
-    end;
-    if (inventory = true) then
-      begin
-        if (count + 1 = select) then
-            begin
-               couleurs(0, 15);
-               pos.x := 58;
-               pos.y := 27;
-               ecrireEnPosition(pos, 'Menu');
-               couleurs(15, 0);
-            end
-        else
-            begin
-                couleurs(15, 0);
-                pos.x := 58;
-                pos.y := 27;
-                ecrireEnPosition(pos, 'Menu');
-            end
-         ;
-         pos.x := 3;
-         pos.y := 2;
-         for i := Low(inv) to High(inv) do
-           begin
-             it := getItemInventory(i);
-             if ((it.id > 0) or (it.count > 0)) then
-                begin
-                     if (select = i) then
-                        couleurs(0, 15)
-                     else
-                         couleurs(15, 0)
-                     ;
-                     ecrireEnPosition(pos, concat(tabProduits[tabIdProduits[it.id]].nom, ' x', InttoStr(it.count)));
-                     pos.y := pos.y + 1;
-                end;
-             end;
-         end
-    else
-      begin
-         if (count2 + 1 = select) then
-            begin
-               couleurs(0, 15);
-               pos.x := 58;
-               pos.y := 27;
-               ecrireEnPosition(pos, 'Menu');
-               couleurs(15, 0);
-            end
-        else
-            begin
-                couleurs(15, 0);
-                pos.x := 58;
-                pos.y := 27;
-                ecrireEnPosition(pos, 'Menu');
-            end
-         ;
-         pos.x := 70;
-         pos.y := 2;
-         for i := Low(arm) to High(arm) do
-           begin
-             it := getItemArmoire(i);
-             if ((it.id > 0) or (it.count > 0)) then
-                begin
-                     if (select = i) then
-                        couleurs(0, 15)
-                     else
-                         couleurs(15, 0)
-                     ;
-                     ecrireEnPosition(pos, concat(tabProduits[tabIdProduits[it.id]].nom, ' x', InttoStr(it.count)));
-                     pos.y := pos.y + 1;
-                end;
-             end;
-      end
-    ;
-    couleurs(15, 0);
-    deplacerCurseurXY(0, 0);
   until ch = #13;
-
-  couleurs(0, 15);
-  pos.x := 3;
-  pos.y := 24;
-  ecrireEnPosition(pos, 'Utiliser/Equiper');
-  couleurs(15, 0);
-  pos.x := 3;
-  pos.y := 25;
-  ecrireEnPosition(pos, 'Deplacer');
-  pos.x := 3;
-  pos.y := 26;
-  ecrireEnPosition(pos, 'Suprimer');
-  pos.x := 3;
-  pos.y := 27;
-  ecrireEnPosition(pos, 'Anuler');
-  deplacerCurseurXY(0, 0);
-  select2 := 1;
-  repeat
-    ch := ReadKey;
-    case ch of
-      'P':
-      begin
-        select2 := select2 + 1;
-        if (select2 >= 5) then
-          select2 := 1;
-      end;
-      'H':
-      begin
-        select2 := select - 1;
-        if (select2 <= 0) then
-          select2 := 4;
-      end;
-    end;
-
-    case select2 of
-      1:
-      begin
-        couleurs(0, 15);
-        pos.x := 3;
-        pos.y := 24;
-        ecrireEnPosition(pos, 'Utiliser/Equiper');
-        couleurs(15, 0);
-        pos.x := 3;
-        pos.y := 25;
-        ecrireEnPosition(pos, 'Deplacer');
-        pos.x := 3;
-        pos.y := 26;
-        ecrireEnPosition(pos, 'Suprimer');
-        pos.x := 3;
-        pos.y := 27;
-        ecrireEnPosition(pos, 'Anuler');
-        deplacerCurseurXY(0, 0);
-      end;
-      2:
-      begin
-        couleurs(0, 15);
-        pos.x := 3;
-        pos.y := 25;
-        ecrireEnPosition(pos, 'Deplacer');
-        couleurs(15, 0);
-        pos.x := 3;
-        pos.y := 24;
-        ecrireEnPosition(pos, 'Utiliser/Equiper');
-        pos.x := 3;
-        pos.y := 26;
-        ecrireEnPosition(pos, 'Suprimer');
-        pos.x := 3;
-        pos.y := 27;
-        ecrireEnPosition(pos, 'Anuler');
-        deplacerCurseurXY(0, 0);
-      end;
-      3:
-      begin
-        couleurs(0, 15);
-        pos.x := 3;
-        pos.y := 26;
-        ecrireEnPosition(pos, 'Suprimer');
-        couleurs(15, 0);
-        pos.x := 3;
-        pos.y := 25;
-        ecrireEnPosition(pos, 'Deplacer');
-        pos.x := 3;
-        pos.y := 24;
-        ecrireEnPosition(pos, 'Utiliser/Equiper');
-        pos.x := 3;
-        pos.y := 27;
-        ecrireEnPosition(pos, 'Anuler');
-        deplacerCurseurXY(0, 0);
-      end;
-      4:
-      begin
-        couleurs(0, 15);
-        pos.x := 3;
-        pos.y := 27;
-        ecrireEnPosition(pos, 'Anuler');
-        couleurs(15, 0);
-        pos.x := 3;
-        pos.y := 26;
-        ecrireEnPosition(pos, 'Suprimer');
-        pos.x := 3;
-        pos.y := 25;
-        ecrireEnPosition(pos, 'Deplacer');
-        pos.x := 3;
-        pos.y := 24;
-        ecrireEnPosition(pos, 'Utiliser/Equiper');
-        deplacerCurseurXY(0, 0);
-      end;
-    end;
-  until ch = #13;
-  //inventory
-  //posinv[select];
-  //posarm[select];
-  readln();
   menuArmoire:=select;
 end;
 
