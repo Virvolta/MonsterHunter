24d38e65a9a59990a731186cef4cfcea556ef7f1
Virvolta <33796736+Virvolta@users.noreply.github.com>
2021-12-04 21:40:40 +0100

    fix syntaxe

--- outils.pas
+++ outils.pas
@@ -8,6 +8,7 @@ uses
   fpjson, jsonparser;
 
 type // type qui nous permet d'enregistrer les caractéristique des produits, des équipements, des monstres, des items et de la cantines
+
   produit=record
     id:Integer;
     nom:String;
@@ -78,124 +79,162 @@ uses
 
 // cette procedure nous permet d'utiliser les fichers ascii
 procedure ascii(nom : string; x, y : integer);
-    var
-       fichier : textFile;
-       ligne : string;
-       compteurLigne : integer;
-       i : Integer;
-    begin
-      assignFile(fichier, concat('ascii/', nom, '.ascii'));
-      reset(fichier);
-      compteurLigne := 0;
-      repeat
-        readln(fichier,ligne);
-        deplacerCurseurXY(x,y+compteurLigne);
-        for i := 1 to length(ligne) do
-            case ligne[i] of
-                  '1' : write(char(176));
-                  '2' : write(char(177));
-                  '3' : write(char(178));
-                  '4' : write(char(219));
-            else write(ligne[i])
-            end;
-        compteurLigne := compteurLigne +1
-      until(EOF(fichier));
-      closeFile(fichier);
-    end;
+
+var
+
+  fichier : textFile;
+  ligne : string;
+  compteurLigne : integer;
+  i : Integer;
+
+begin
+
+  assignFile(fichier, concat('ascii/', nom, '.ascii'));
+  reset(fichier);
+  compteurLigne := 0;
+
+  repeat
+
+      readln(fichier,ligne);
+      deplacerCurseurXY(x,y+compteurLigne);
+
+      for i := 1 to length(ligne) do
+          case ligne[i] of
+              '1' : write(char(176));
+              '2' : write(char(177));
+              '3' : write(char(178));
+              '4' : write(char(219));
+          else write(ligne[i])
+          end;
+
+      compteurLigne := compteurLigne +1
+
+  until(EOF(fichier));
+
+  closeFile(fichier);
+
+end;
 
 // cette procedure permet de faire ce deplacer un petit carré bleu pour simuler le deplacement du joueur
 procedure animationdeplacement(x, y, x2, y2 : integer);
+
 var
-   contsec: string;
-   ch : char;
-   anime : boolean;
-   move : Integer;
-   pos: coordonnees;
+
+  anime : boolean;
+
 begin
+
   x2 := x + (x2 *2);
   y2 := y + (y2 *2);
   anime := true;
-  contsec := '-1';
-  repeat
-    attendre(50);
 
-    couleurs(15, 0);
-    deplacerCurseurXY(x, y);
-    write(char(176),char(176));
-    if ((x = x2) and (y = y2)) then
-       begin
-         anime := false;
-       end
-    else if (x = x2) then
-       begin
+  repeat
+
+      attendre(50);
+
+      couleurs(15, 0);
+      deplacerCurseurXY(x, y);
+      write(char(176),char(176));
+
+      if ((x = x2) and (y = y2)) then
+        begin
+
+            anime := false;
+
+        end
+      else if (x = x2) then
+        begin
+
             if (y > y2) then
                y := y - 2
             else
-                y := y + 2;
-       end
-    else
+                y := y + 2
+            ;
+        end
+      else
         begin
              if (x > x2) then
                 x := x - 2
              else
-                 x := x + 2;
-       end;
-    ;
-    couleurs(3, 0);
-    deplacerCurseurXY(x, y);
-    write(char(219),char(219));
+                 x := x + 2
+             ;
+        end
+      ;
+
+      couleurs(3, 0);
+      deplacerCurseurXY(x, y);
+      write(char(219),char(219));
 
   until anime = false;
+
 end;
 
 // cette procedure nous permet d'utiliser des fichier json
 procedure importObjData(data : TJSONData ; ObjName : String);
- var
-   obj : TJSONObject;
-   tab : TJSONArray;
-   i : byte;
+
+var
+
+  obj : TJSONObject;
+  tab : TJSONArray;
+  i : byte;
 
 begin
+
   obj :=TJSONObject(data);
+
   if ObjName='objets' then
-  begin
-     tab:=obj.Arrays[ObjName];
-     for i:=0 to tab.Count-1 do
-     begin
-       obj:= tab.Objects[i];
-       tabProduits[i].id:=StrToInt(obj.Strings['id']);
-       tabProduits[i].nom:=obj.Strings['nom'];
-       tabProduits[i].prixAchat:=StrToInt(obj.Strings['prixAchat']);
-       tabProduits[i].prixVente:=StrToInt(obj.Strings['prixVente']);
-       tabIdProduits[StrToInt(obj.Strings['id'])] := i;
-     end;
-  end
+    begin
+
+        tab:=obj.Arrays[ObjName];
+
+        for i:=0 to tab.Count-1 do
+          begin
+
+              obj:= tab.Objects[i];
+              tabProduits[i].id:=StrToInt(obj.Strings['id']);
+              tabProduits[i].nom:=obj.Strings['nom'];
+              tabProduits[i].prixAchat:=StrToInt(obj.Strings['prixAchat']);
+              tabProduits[i].prixVente:=StrToInt(obj.Strings['prixVente']);
+              tabIdProduits[StrToInt(obj.Strings['id'])] := i;
+
+          end;
+     end
   else if ObjName='equipements' then
-  begin
-     tab:=obj.Arrays['equipements'];
-     for i:=0 to tab.Count-1 do
-     begin
-       obj:= tab.Objects[i];
-       tabEquipments[i].id:=StrToInt(obj.Strings['id']);
-       tabEquipments[i].nom:=obj.Strings['nom'];
-       tabEquipments[i].stat:=StrToInt(obj.Strings['stat']);
-       tabEquipments[i].slot:=StrToInt(obj.Strings['slot']);
-       tabIdEquipments[StrToInt(obj.Strings['id'])] := i;
-     end;
-  end
+    begin
+
+        tab:=obj.Arrays['equipements'];
+
+        for i:=0 to tab.Count-1 do
+          begin
+
+              obj:= tab.Objects[i];
+              tabEquipments[i].id:=StrToInt(obj.Strings['id']);
+              tabEquipments[i].nom:=obj.Strings['nom'];
+              tabEquipments[i].stat:=StrToInt(obj.Strings['stat']);
+              tabEquipments[i].slot:=StrToInt(obj.Strings['slot']);
+              tabIdEquipments[StrToInt(obj.Strings['id'])] := i;
+
+          end;
+    end
   else if ObjName='cantines' then
-  begin
-     tab:=obj.Arrays['cantines'];
-     for i:=0 to tab.Count-1 do
-     begin
-       obj:= tab.Objects[i];
-       tabCantines[i].nom:=obj.Strings['nom'];
-       tabCantines[i].prixAchat:=StrToInt(obj.Strings['prixAchat']);
-       tabCantines[i].avantage:=StrToInt(obj.Strings['avantage']);
-     end;
-  end
+    begin
+
+        tab:=obj.Arrays['cantines'];
+
+        for i:=0 to tab.Count-1 do
+          begin
+
+              obj:= tab.Objects[i];
+              tabCantines[i].nom:=obj.Strings['nom'];
+              tabCantines[i].prixAchat:=StrToInt(obj.Strings['prixAchat']);
+              tabCantines[i].avantage:=StrToInt(obj.Strings['avantage']);
+
+          end;
+    end
   else
-      writeln('Aucun objet de ce type dans le fichier JSON');
+      writeln('Aucun objet de ce type dans le fichier JSON')
+  ;
+
 end;
 
 // cette procedure nous permet d'importer les montres depuis un fichier json
@@ -208,55 +247,72 @@ var
    i : byte;
 
 begin
+
   obj :=TJSONObject(data);
   tab:=obj.Arrays[ObjName];
+
   for i:=0 to tab.Count-1 do
-  begin
-    obj:= tab.Objects[i];
-    tabMonstre[i].niveau:=StrToInt(obj.Strings['niveau']);
-    tabMonstre[i].nom:=obj.Strings['nom'];
-    tabMonstre[i].hp:=StrToInt(obj.Strings['HP']);
-    tabMonstre[i].degatmin:=StrToInt(obj.Strings['degatmin']);
-    tabMonstre[i].degatmax:=StrToInt(obj.Strings['degatmax']);
-    tabMonstre[i].defensemin:=StrToInt(obj.Strings['defensemin']);
-    tabMonstre[i].defensemax:=StrToInt(obj.Strings['defensemax']);
-  end;
+    begin
+
+        obj:= tab.Objects[i];
+        tabMonstre[i].niveau:=StrToInt(obj.Strings['niveau']);
+        tabMonstre[i].nom:=obj.Strings['nom'];
+        tabMonstre[i].hp:=StrToInt(obj.Strings['HP']);
+        tabMonstre[i].degatmin:=StrToInt(obj.Strings['degatmin']);
+        tabMonstre[i].degatmax:=StrToInt(obj.Strings['degatmax']);
+        tabMonstre[i].defensemin:=StrToInt(obj.Strings['defensemin']);
+        tabMonstre[i].defensemax:=StrToInt(obj.Strings['defensemax']);
+
+    end;
 end;
 
 // cette procedure affiche les données json
 Procedure DoParse(Parseur : TJSONParser ; ObjName : String);
-Var
+
+var
+
   js : TJSONData;
+
 begin
+
   js:=parseur.Parse;
+
   if (ObjName='objets') or (ObjName='equipements') or (ObjName='cantines') then
      importObjData(js,ObjName)
   else
-      importMonsterData(js,ObjName);
+      importMonsterData(js,ObjName)
+  ;
+
 end;
 
 // cette procedure créé des parties dans les fichiers json
 Procedure ParseFile (FileName, ObjName : String);
-Var
+
+var
+
   flux : TFileStream;
   Parseur : TJSONParser;
+
 begin
-  If FileExists(FileName) then
-  begin
-    flux:=TFileStream.create(FileName, fmopenRead);
-      try
-        parseur:=TJSONParser.create(flux);
+
+  if FileExists(FileName) then
+    begin
+        flux:=TFileStream.create(FileName, fmopenRead);
         try
-          DoParse(parseur, ObjName);
-        finally
-          FreeAndNil(parseur);
-        end;
-      finally
-        flux.Destroy;
-      end;
-  end
+            parseur:=TJSONParser.create(flux);
+
+            try
+                DoParse(parseur, ObjName);
+            finally
+                FreeAndNil(parseur);
+            end;
+         finally
+             flux.Destroy;
+         end;
+    end
   else
-      writeln('Le fichier JSON : ', FileName,' n''existe pas');
+      writeln('Le fichier JSON : ', FileName,' n''existe pas')
+  ;
 end;
 
 end.
